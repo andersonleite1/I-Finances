0736ed8b8fb157ef7ee91c9f75c17837a3182b5d
Anderson Leite <andersonleite.dev@gmail.com>
2022-10-17 11:23:42 -0300

    feat(components): create component Dashboard

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Summary from '../Summary';
+import { Main } from './styles';
+
+export default function Dashboard() {
+  return (
+    <Main>
+      <Summary />
+    </Main>
+  );
+}
