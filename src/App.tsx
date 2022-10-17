5e9e0c4c6efd79219864518e8f1dac61a4c8ec02
Anderson Leite <andersonleite.dev@gmail.com>
2022-10-17 09:53:21 -0300

    feat: add component Header at App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Start project</h1>
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
   );
 }
 
