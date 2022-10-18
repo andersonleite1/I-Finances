7b8ea8b939094b84942e293310521523125c29c1
Anderson Leite <andersonleite.dev@gmail.com>
2022-10-17 21:27:49 -0300

    feat(components):  add component TransactionsTable at Dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Summary from '../Summary';
+import TransactionsTable from '../TransactionsTable';
 import { Main } from './styles';
 
 export default function Dashboard() {
   return (
     <Main>
       <Summary />
+      <TransactionsTable />
     </Main>
   );
 }
