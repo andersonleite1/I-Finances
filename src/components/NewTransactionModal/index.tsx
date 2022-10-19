9d4393ce93c1a1ae683b868e6834b999f83905d1
Anderson Leite <andersonleite.dev@gmail.com>
2022-10-18 22:08:33 -0300

    feat(components): add inputs and button at modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from 'react-modal';
+import Container from './style';
 
 type NewTransactionModalProps = {
   isOpen: boolean;
@@ -11,8 +12,16 @@ export default function NewTransactionModal({ isOpen, onRequestClose }: NewTrans
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
       <h2>Cadastrar transação</h2>
+      <Container>
+        <input placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
