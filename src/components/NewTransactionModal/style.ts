4c2322f1247a70e20fd4c03b86dc48c280e23ab6
Anderson Leite <andersonleite.dev@gmail.com>
2022-10-23 23:29:19 -0300

    style: add style component RadioBox

--- src/components/NewTransactionModal/style.ts
+++ src/components/NewTransactionModal/style.ts
@@ -45,28 +45,33 @@ export const TransactionTypeContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
-  button {
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
-    background: transparent;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition: border-color background-color 0.3s;
-    &:hover {
-      border-color: #aaa;
-      background-color: #eee;
-    }
-    img {
-      width: 20px;
-      height: 20px;
-    }
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+`;
+
+interface RadioBoxProps {
+  isActive: boolean;
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
+  background: ${(props) => (props.isActive ? '#ccc' : 'transparent')};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: border-color background-color 0.3s;
+  &:hover {
+    border-color: #aaa;
+    background-color: #eee;
+  }
+  img {
+    width: 20px;
+    height: 20px;
+  }
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `;
