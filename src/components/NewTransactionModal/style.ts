b735606d3079bd8782e9350e9313f5b1d2d7db99
Anderson Leite <andersonleite.dev@gmail.com>
2022-10-25 01:12:57 -0300

    style: add style component RadioBox

--- src/components/NewTransactionModal/style.ts
+++ src/components/NewTransactionModal/style.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
   h2 {
@@ -49,20 +50,30 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
   isActive: boolean;
+  activeColor: 'green' | 'red';
 }
+
+const colors = {
+  green: '#499F78',
+  red: '#E0062B',
+};
 
 export const RadioBox = styled.button<RadioBoxProps>`
   height: 4rem;
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
-  background: ${(props) => (props.isActive ? '#ccc' : 'transparent')};
+
+  background: ${(props) => (props.isActive
+    ? transparentize(0.9, colors[props.activeColor])
+    : 'transparent'
+  )};
+  
   display: flex;
   align-items: center;
   justify-content: center;
   transition: border-color background-color 0.3s;
   &:hover {
-    border-color: #aaa;
-    background-color: #eee;
+    border-color: ${darken(0.1, '#d7d7d7')};
   }
   img {
     width: 20px;
